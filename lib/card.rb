3f919abab750608976e716d403b2344269030ba1
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-04-29 15:52:29 -0600

    Create initial possible layout

---
+++ lib/card.rb
@@ -0,0 +1,18 @@
+class Card
+
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
+  end
+
+  def question(question)
+  end
+
+  def answer(answer)
+  end
+
+  def category(category)
+  end
+
+end
