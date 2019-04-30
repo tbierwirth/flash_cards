4b9683db91bea0fa088d5d4428dbdcb15f67f002
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-04-30 13:12:17 -0600

    Setup initialize method correctly

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,18 @@
+require './lib/card.rb'
+
 class Turn
+  attr_reader :string, :card
+
   def initialize(string, card)
+    @guess = string
+    @card = card
   end
 
   def guess
   end
 
   def card
+
   end
 
   def correct?
