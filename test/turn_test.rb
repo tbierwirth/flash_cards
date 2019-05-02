0f39befe63b14d09acb063c7b10ab149e69b6303
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-02 15:07:13 -0600

    Organize instances into setup method

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -19,6 +19,13 @@ class TurnTest < Minitest::Test
     assert_equal "Juneau", turn.guess
   end
 
+  def test_turn_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal card, turn.card
+  end
+
   def test_turn_correct?
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
